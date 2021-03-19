3adc0ada7bce09186431e0a8f2657c227d7ec9e8
mahmut alolo <mahmut.alolo@hotmail.com>
2021-03-19 18:17:15 +0300

    Add project files.

---
+++ ProjectMahmut/WebForm1.aspx.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace ProjectMahmut
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            
+
+        }
+
+        protected void GridView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void DetailsView1_PageIndexChanging(object sender, DetailsViewPageEventArgs e)
+        {
+
+        }
+
+        protected void SqlDataSource3_Selecting(object sender, SqlDataSourceSelectingEventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            lblUyari.Visible = true;
+            lblUyari.Text = "Program Kurulum yaparken sorun yaşadım, özellikle TELERİK ile ilgili çok sorun yaşadım, kurlulum düzelince daha hızlı ilerliceğim";
+        }
+    }
+}
